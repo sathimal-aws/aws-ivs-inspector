bab8e3a5118b68df09c26ed56acd1dd7eb395619
Sathia Saithanya Malli Mohanram <sathimal@amazon.com>
2024-09-11 07:52:58 +0800

    apigateway API without authorization-10

--- terraform-infra/modules/aws-infra/1-apigateway-rest.tf
+++ terraform-infra/modules/aws-infra/1-apigateway-rest.tf
@@ -58,11 +58,6 @@ resource "aws_api_gateway_integration_response" "integration_response" {
   resource_id = aws_api_gateway_resource.resource[each.key].id
   http_method = aws_api_gateway_method.method[each.key].http_method
   status_code = aws_api_gateway_method_response.method_response[each.key].status_code
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-    "method.response.header.Access-Control-Allow-Methods" = "GET,OPTIONS,POST,PUT",
-    "method.response.header.Access-Control-Allow-Origin"  = "*"
-  }
   depends_on = [
     aws_api_gateway_method.method,
     aws_api_gateway_integration.integration
@@ -77,7 +72,9 @@ resource "aws_api_gateway_method_response" "method_response" {
   http_method = aws_api_gateway_method.method[each.key].http_method
   status_code = "200"
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
   }
   depends_on = [
     aws_api_gateway_method.method,
