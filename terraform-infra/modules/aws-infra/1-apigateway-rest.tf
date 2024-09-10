1f98f6c07e089c8917be8ef2b1bbcec67b9997f2
Sathia Saithanya Malli Mohanram <sathimal@amazon.com>
2024-09-11 07:48:14 +0800

    apigateway API without authorization-10

--- terraform-infra/modules/aws-infra/1-apigateway-rest.tf
+++ terraform-infra/modules/aws-infra/1-apigateway-rest.tf
@@ -59,9 +59,9 @@ resource "aws_api_gateway_integration_response" "integration_response" {
   http_method = aws_api_gateway_method.method[each.key].http_method
   status_code = aws_api_gateway_method_response.method_response[each.key].status_code
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+    "method.response.header.Access-Control-Allow-Methods" = "GET,OPTIONS,POST,PUT",
+    "method.response.header.Access-Control-Allow-Origin"  = "*"
   }
   depends_on = [
     aws_api_gateway_method.method,
