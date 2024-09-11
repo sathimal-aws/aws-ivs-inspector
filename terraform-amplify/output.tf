190ba83731d70939215add5dad4a93c618f7165d
Sathia Saithanya Malli Mohanram <sathimal@amazon.com>
2024-09-11 23:19:13 +0800

    apigateway API without authorization-16

--- terraform-amplify/output.tf
+++ terraform-amplify/output.tf
@@ -2,17 +2,13 @@ output "amplify_app_id" {
   value = aws_amplify_app.app.id
 }
 
-# output "cognito_identity_pool_id" {
-#   value = aws_cognito_identity_pool.identity_pool.id
-# }
+output "cognito_identity_pool_id" {
+  value = aws_cognito_identity_pool.identity_pool.id
+}
 
-# output "cognito_user_pool_id" {
-#   value = aws_cognito_user_pool.user_pool.id
-# }
-
-# output "cognito_app_client_id" {
-#   value = aws_cognito_user_pool_client.user_pool_client.id
-# }
+output "cognito_user_pool_id" {
+  value = aws_cognito_user_pool.user_pool.id
+}
 
 output "cognito_ids" {
   value = jsonencode({
