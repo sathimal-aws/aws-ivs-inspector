a2c819d555f9bdcb3c7c0bdfb26a13ad75ea099d
Sathia Saithanya Malli Mohanram <sathimal@amazon.com>
2024-09-12 03:51:04 +0800

    apigateway API without authorization-27

--- terraform-infra/modules/aws-infra/1-apigateway-rest.tf
+++ terraform-infra/modules/aws-infra/1-apigateway-rest.tf
@@ -33,13 +33,13 @@ resource "aws_api_gateway_method" "method" {
   authorizer_id = aws_api_gateway_authorizer.authorizer.id
 }
 
-# resource "aws_api_gateway_method" "options_method" {
-#   for_each      = { for api in var.rest_apis : api.name => api }
-#   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-#   resource_id   = aws_api_gateway_resource.resource[each.key].id
-#   http_method   = "OPTIONS"
-#   authorization = "NONE"
-# }
+resource "aws_api_gateway_method" "options_method" {
+  for_each      = { for api in var.rest_apis : api.name => api }
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.resource[each.key].id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
 
 # integrate with Lambda
 resource "aws_api_gateway_integration" "integration" {
@@ -58,14 +58,14 @@ resource "aws_api_gateway_integration" "integration" {
   ]
 }
 
-# resource "aws_api_gateway_integration" "options_integration" {
-#   for_each    = { for api in var.rest_apis : api.name => api }
-#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-#   resource_id = aws_api_gateway_resource.resource[each.key].id
-#   http_method = aws_api_gateway_method.options_method[each.key].http_method
-#   type        = "MOCK"
-#   depends_on  = [aws_api_gateway_method.options_method]
-# }
+resource "aws_api_gateway_integration" "options_integration" {
+  for_each    = { for api in var.rest_apis : api.name => api }
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.resource[each.key].id
+  http_method = aws_api_gateway_method.options_method[each.key].http_method
+  type        = "MOCK"
+  depends_on  = [aws_api_gateway_method.options_method]
+}
 
 # integration response from Lambda
 resource "aws_api_gateway_integration_response" "integration_response" {
@@ -76,7 +76,7 @@ resource "aws_api_gateway_integration_response" "integration_response" {
   status_code = aws_api_gateway_method_response.method_response[each.key].status_code
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-    "method.response.header.Access-Control-Allow-Methods" = "GET,POST,PUT",
+    "method.response.header.Access-Control-Allow-Methods" = "GET,OPTIONS,POST,PUT",
     "method.response.header.Access-Control-Allow-Origin"  = "*"
   }
   depends_on = [
@@ -86,19 +86,19 @@ resource "aws_api_gateway_integration_response" "integration_response" {
   ]
 }
 
-# resource "aws_api_gateway_integration_response" "options_integration_response" {
-#   for_each    = { for api in var.rest_apis : api.name => api }
-#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-#   resource_id = aws_api_gateway_resource.resource[each.key].id
-#   http_method = aws_api_gateway_method.options_method[each.key].http_method
-#   status_code = aws_api_gateway_method_response.options_method_response[each.key].status_code
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Headers" = "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-#     "method.response.header.Access-Control-Allow-Methods" = "GET,OPTIONS,POST,PUT",
-#     "method.response.header.Access-Control-Allow-Origin"  = "*"
-#   }
-#   depends_on = [aws_api_gateway_method_response.options_method_response]
-# }
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  for_each    = { for api in var.rest_apis : api.name => api }
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.resource[each.key].id
+  http_method = aws_api_gateway_method.options_method[each.key].http_method
+  status_code = aws_api_gateway_method_response.options_method_response[each.key].status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+    "method.response.header.Access-Control-Allow-Methods" = "GET,OPTIONS,POST,PUT",
+    "method.response.header.Access-Control-Allow-Origin"  = "*"
+  }
+  depends_on = [aws_api_gateway_method_response.options_method_response]
+}
 
 # method response from Lambda that passes back to Client application
 resource "aws_api_gateway_method_response" "method_response" {
@@ -120,23 +120,23 @@ resource "aws_api_gateway_method_response" "method_response" {
   ]
 }
 
-# resource "aws_api_gateway_method_response" "options_method_response" {
-#   for_each    = { for api in var.rest_apis : api.name => api }
-#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-#   resource_id = aws_api_gateway_resource.resource[each.key].id
-#   http_method = aws_api_gateway_method.options_method[each.key].http_method
-#   status_code = "200"
+resource "aws_api_gateway_method_response" "options_method_response" {
+  for_each    = { for api in var.rest_apis : api.name => api }
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.resource[each.key].id
+  http_method = aws_api_gateway_method.options_method[each.key].http_method
+  status_code = "200"
 
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Headers" = true,
-#     "method.response.header.Access-Control-Allow-Methods" = true,
-#     "method.response.header.Access-Control-Allow-Origin"  = true
-#   }
-#   response_models = {
-#     "application/json" = "Empty"
-#   }
-#   depends_on = [aws_api_gateway_method.options_method]
-# }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+  response_models = {
+    "application/json" = "Empty"
+  }
+  depends_on = [aws_api_gateway_method.options_method]
+}
 
 # deploy the API
 resource "aws_api_gateway_deployment" "deployment" {
@@ -156,7 +156,7 @@ resource "aws_api_gateway_deployment" "deployment" {
     aws_api_gateway_method_response.method_response,
     aws_api_gateway_integration.integration,
     aws_api_gateway_integration_response.integration_response,
-    # aws_api_gateway_integration_response.options_integration_response,
+    aws_api_gateway_integration_response.options_integration_response,
     aws_lambda_function.lambda_function,
   ]
 }
